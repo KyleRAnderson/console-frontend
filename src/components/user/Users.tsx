1a98713949700b5ad35fbac6afba5a55654493fd
Kyle Anderson <kra.aue@gmail.com>
2020-04-22 12:14:53 -0400

    Improve users
    All user information is now stored in a table, with delete button right next to it. No need for a
    separate component to show users.

--- src/components/user/Users.tsx
+++ src/components/user/Users.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Redirect, Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import User from '../../models/User';
 import * as appRoutes from '../../routes/AppLocations';
 import * as apiRoutes from '../../routes/ApiPaths';
@@ -12,7 +12,7 @@ type State = {
 class UserList extends React.Component<any, State> {
     state = {
         toHome: false,
-        users: [],
+        users: Array<User>(),
     };
 
     constructor(props: any) {
@@ -20,6 +20,10 @@ class UserList extends React.Component<any, State> {
     }
 
     componentDidMount() {
+        this.loadUsers();
+    }
+
+    loadUsers(): void {
         fetch(apiRoutes.usersIndexPath)
             .then((response) => {
                 if (response.ok) {
@@ -37,25 +41,50 @@ class UserList extends React.Component<any, State> {
         }
 
         const users = this.state.users;
-        const allUsersDisplay = users.map((user: User) => {
+        const allUsersDisplay: JSX.Element[] = users.map((user: User) => {
             return (
-                <div key={user.id} className="col-md-6 col-lg-4">
-                    <div className="card-body">
-                        <h5 className="card-title">{user.username}</h5>
-                        <Link to={`${appRoutes.usersRoot}/${user.id}`} className="btn custom-button">
-                            User Details
-                        </Link>
-                    </div>
-                </div>
+                <tr key={user.id} className="">
+                    <td>{user.username}</td>
+                    <td>{user.email}</td>
+                    <td>
+                        <button type="button" className="btn btn-danger" onClick={() => this.deleteUser(user.id)}>
+                            Delete
+                        </button>
+                    </td>
+                </tr>
             );
         });
 
         return (
             <div>
                 <h1>Users</h1>
-                {allUsersDisplay}
+                <table>
+                    <tr>
+                        <th>Username</th>
+                        <th>Email</th>
+                    </tr>
+                    {allUsersDisplay}
+                </table>
             </div>
         );
+    }
+
+    deleteUser(id: string): void {
+        const token: string | undefined = document.querySelector<HTMLMetaElement>('meta[name="csrf-token"]')?.content;
+        if (token) {
+            fetch(`${apiRoutes.usersDeletePath}/${id}`, {
+                method: 'DELETE',
+                headers: {
+                    'X-CSRF-Token': token,
+                    'Content-Type': 'application/json',
+                },
+            }).then((response) => {
+                if (response.ok) {
+                    const newUsersArray: Array<User> = this.state.users.filter((user) => user.id !== id);
+                    this.setState(Object.assign({}, this.state, { users: newUsersArray }));
+                }
+            });
+        }
     }
 }
 
--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../components/Home';
 import UserList from '../components/user/Users';
-import UserShow from '../components/user/UserShow';
 import * as appPaths from './AppLocations';
 
 export default (
-  <Router>
-    <Switch>
-      <Route path={appPaths.root} exact component={Home} />
-      <Route path={appPaths.usersRoot} exact component={UserList} />
-      <Route path={appPaths.usersRoot + '/:id'} exact component={UserShow} />
-    </Switch>
-  </Router>
+    <Router>
+        <Switch>
+            <Route path={appPaths.root} exact component={Home} />
+            <Route path={appPaths.usersRoot} exact component={UserList} />
+        </Switch>
+    </Router>
 );
