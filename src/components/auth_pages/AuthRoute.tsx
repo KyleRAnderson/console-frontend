5166c688d057c5e110f027dbfb30e5ef6f5d8788
Kyle Anderson <kra.aue@gmail.com>
2020-05-11 20:48:27 -0400

    Implement authentication timeout frontend messages.
    The frontend is now aware when the authentication token has expired and redirect appropriately.

--- src/components/auth_pages/AuthRoute.tsx
+++ src/components/auth_pages/AuthRoute.tsx
@@ -1,18 +1,34 @@
 // Pages involved in authentication or that need authentication in order to be accessed.
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route, Redirect, RouteProps } from 'react-router-dom';
 import AppPaths from '../../routes/AppLocations';
 import Auth from '../../auth';
+import Notifications from '../../notification';
 
 export class AuthRoute extends Route {}
 
 function authRoute<T extends RouteProps>({ children, component, render, ...rest }: T): JSX.Element {
+    const [authenticated, setAuthenticated] = useState(Auth.isLoggedIn());
+
+    const onAuthFailure: Auth.AuthFailureSubscriber = () => {
+        setAuthenticated(Auth.isLoggedIn());
+        Notifications.createNotification({ message: 'Session has Expired', type: 'warning' });
+    };
+
+    useEffect(() => {
+        Auth.onAuthFailure(onAuthFailure);
+
+        return function cleanup() {
+            Auth.removeAuthFailureSubscription(onAuthFailure);
+        };
+    }, []);
+
     return (
         <Route
             {...rest}
             render={(props) => {
-                if (Auth.isLoggedIn()) {
+                if (authenticated) {
                     // This is the same priority given to the regular route component.
                     if (children) {
                         return children;
