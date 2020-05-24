44e7571ae72f30dedb74ed704d8bbd47322ef26a
Kyle Anderson <kra.aue@gmail.com>
2020-05-24 13:42:03 -0400

    Add more typings to notifications module
    The notifications module now has all of the parameters available for the
    react-notifications-component library included.

--- src/notification.ts
+++ src/notification.ts
@@ -1,9 +1,17 @@
 import { store } from 'react-notifications-component';
 
 namespace Notifications {
-    export interface NotificationProperties {
+    type Transition = {
+        duration?: number;
+        timingFunction?: string;
+        delay?: number;
+    };
+    export type NotificationConfig = {
+        id?: string;
+        onRemoval?: (id: string, removedBy: any) => void;
         title?: string;
-        message?: string;
+        message: string;
+        content?: React.Component | JSX.Element;
         type?: 'success' | 'danger' | 'info' | 'default' | 'warning';
         container?:
             | 'top-left'
@@ -13,24 +21,40 @@ namespace Notifications {
             | 'bottom-left'
             | 'bottom-right'
             | 'bottom-center';
+        insert?: 'top' | 'bottom';
+        dismiss?: {
+            duration?: number;
+            onScreen?: boolean;
+            pauseOnHover?: boolean;
+            waitForAnimation?: boolean;
+            click?: boolean;
+            touch?: boolean;
+            showIcon?: boolean;
+        };
         animationIn?: string[];
         animationOut?: string[];
-    }
+        slidingEnter?: Transition;
+        slidingExit?: Transition;
+        touchRevert?: Transition;
+        touchSlidingExit?: Transition;
+        width?: number;
+    };
 
-    export const defaultDuration: number = 3500;
+    const defaultConfig: Partial<NotificationConfig> = {
+        insert: 'top',
+        container: 'top-right',
+        animationIn: ['animated', 'fadeIn'],
+        animationOut: ['animated', 'fadeOut'],
+        dismiss: {
+            duration: 3500,
+            pauseOnHover: true,
+            onScreen: true,
+            showIcon: true,
+        },
+    };
 
-    export function createNotification(config: NotificationProperties) {
-        store.addNotification({
-            insert: 'top',
-            container: 'top-right',
-            animationIn: ['animated', 'fadeIn'],
-            animationOut: ['animated', 'fadeOut'],
-            dismiss: {
-                duration: defaultDuration,
-                onScreen: true,
-            },
-            ...config,
-        });
+    export function createNotification(config: NotificationConfig) {
+        store.addNotification({ ...defaultConfig, ...config });
     }
 }
 export default Notifications;
