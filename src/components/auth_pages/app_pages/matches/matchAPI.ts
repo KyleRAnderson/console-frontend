4b3109cd5916d4c6f4734307e59eddecc9200789
Kyle Anderson <kra.aue@gmail.com>
2020-05-30 17:48:22 -0400

    Add match API frontend module

---
+++ src/components/auth_pages/app_pages/matches/matchAPI.ts
@@ -0,0 +1,30 @@
+import PaginatedResponse from '../../../../models/PaginatedResponse';
+import Match, { MatchBase } from '../../../../models/Match';
+import ApiRequest from '../../../../apiRequests';
+import ApiPaths from '../../../../routes/ApiPaths';
+import Hunt from '../../../../models/Hunt';
+import { AxiosResponse } from 'axios';
+
+namespace MatchAPI {
+    type PaginatedMatches = PaginatedResponse & {
+        matches: Match[];
+    };
+
+    export function getMatches(hunt: Hunt | string, pageParams: ApiRequest.PaginationParams) {
+        return ApiRequest.getItem<PaginatedMatches>(ApiPaths.matchesPath(hunt), { params: pageParams });
+    }
+
+    export function getMatch(hunt: Hunt | string, matchNumber: number): Promise<AxiosResponse<Match>> {
+        return ApiRequest.getItem<Match>(ApiPaths.matchPath(hunt, matchNumber));
+    }
+
+    export function deleteMatch(hunt: Hunt | string, matchNumber: number): Promise<AxiosResponse> {
+        return ApiRequest.deleteItem(ApiPaths.matchPath(hunt, matchNumber));
+    }
+
+    export function postMatch(hunt: Hunt | string, match: MatchBase): Promise<AxiosResponse<Match>> {
+        return ApiRequest.postItem<MatchBase, Match>(ApiPaths.matchesPath(hunt), match);
+    }
+}
+
+export default MatchAPI;
