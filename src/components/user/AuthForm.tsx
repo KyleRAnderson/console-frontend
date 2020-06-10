aed6c7d0a9bfb12f86e2c1bb1eec91b2cdaa2527
Kyle Anderson <kra.aue@gmail.com>
2020-06-09 23:13:41 -0400

    Improve AuthForm validation and types
    Some more of the AuthForm typings have been made more accessible by
    other libraries.
    
    I've also made the validation function when the form is empty, as before
    it assumed by default that the input was valid. It now checks
    explicitly, and handles not receiving a validation prop by assuming that
    the input value is valid (correct behaviour).
    
    Now also keep track of whether or not the user has submitted. Once the
    user has submitted, empty input values will show errors.

--- src/components/user/AuthForm.tsx
+++ src/components/user/AuthForm.tsx
@@ -14,6 +14,7 @@ type FieldProperty = {
     validator?: Validator;
     errorMessage?: string;
 };
+export type AuthData = Map<AcceptedKey, string>;
 
 // From https://emailregex.com/
 export const EMAIL_REGEX: RegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -30,7 +31,8 @@ export const passwordField: FieldProperty = Object.freeze({
     label: 'Password',
     validator: Auth.PasswordValidation.REGEX,
     type: 'password',
-    errorMessage: 'Invalid password.',
+    errorMessage:
+        `Between ${Auth.PasswordValidation.MIN_LENGTH} and ` + `${Auth.PasswordValidation.MAX_LENGTH} characters.`,
     placeholder: 'Password',
 });
 
@@ -39,7 +41,7 @@ export type FieldMappings = Map<AcceptedKey, FieldProperty>;
 type Props = {
     // Using a map here because it preserves insertion order.
     fieldMappings: FieldMappings;
-    onSubmit?: (data: Map<AcceptedKey, string>) => void;
+    onSubmit?: (data: AuthData) => void;
     readonly buttonLabel: string;
     disableSubmit?: boolean;
 };
@@ -48,12 +50,15 @@ export default function AuthForm(props: Props): JSX.Element {
     const initialCredentials = new Map<AcceptedKey, string>();
     const initialErrors = new Map<AcceptedKey, boolean>();
     for (let key of props.fieldMappings.keys()) {
-        initialCredentials.set(key, '');
-        initialErrors.set(key, false);
+        const initialValue: string = '';
+        initialCredentials.set(key, initialValue);
+        initialErrors.set(key, !isValid(props.fieldMappings.get(key)?.validator, initialValue));
     }
-    const [credentials, setCredentials] = useState<Map<AcceptedKey, string>>(initialCredentials);
+    const [credentials, setCredentials] = useState<AuthData>(initialCredentials);
     // Map of form {key: boolean} where the boolean value is true if there is an error.
     const [formErrors, setFormErrors] = useState<Map<AcceptedKey, boolean>>(initialErrors);
+    // Set to true when submitted, just to show errors.
+    const [wasSubmitted, setWasSubmitted] = useState<boolean>(false);
 
     function formIsValid(): boolean {
         return Array.from(formErrors.values()).every((value) => !value);
@@ -64,14 +69,18 @@ export default function AuthForm(props: Props): JSX.Element {
         if (formIsValid()) {
             props.onSubmit?.(credentials);
         }
+        setWasSubmitted(true);
     }
 
-    function isValid(validator: Validator, value: string): boolean {
-        let valid: boolean = false;
-        if (validator instanceof RegExp) {
-            valid = validator.test(value);
-        } else {
-            valid = validator(value);
+    function isValid(validator: Validator | undefined, value: string): boolean {
+        // If no validator is provided, then we are valid by default.
+        let valid: boolean = validator === undefined;
+        if (validator) {
+            if (validator instanceof RegExp) {
+                valid = validator.test(value);
+            } else {
+                valid = validator(value);
+            }
         }
         return valid;
     }
@@ -80,9 +89,7 @@ export default function AuthForm(props: Props): JSX.Element {
         let value: string = event.currentTarget.value;
         setCredentials(new Map(credentials).set(key, value));
         let fieldMapping = props.fieldMappings.get(key);
-        if (fieldMapping?.validator) {
-            setFormErrors(new Map(formErrors).set(key, !isValid(fieldMapping.validator, value)));
-        }
+        setFormErrors(new Map(formErrors).set(key, !isValid(fieldMapping?.validator, value)));
     }
 
     let formElements: JSX.Element[] = Array.from(props.fieldMappings, ([key, mapping]) => {
@@ -95,7 +102,12 @@ export default function AuthForm(props: Props): JSX.Element {
                     value={credentials.get(key)}
                     onChange={(e) => handleValueChanged(key, e as React.FormEvent<HTMLInputElement>)}
                 />
-                <span className="error">{formErrors.get(key) ? props.fieldMappings.get(key)?.errorMessage : null}</span>
+                <span className="error">
+                    {/*If form has been submitted, we should show error. Otherwise, if length is not nothing, show error.*/}
+                    {(wasSubmitted || credentials.get(key)) && formErrors.get(key)
+                        ? props.fieldMappings.get(key)?.errorMessage
+                        : null}
+                </span>
             </Form.Group>
         );
     });
--- src/components/user/ResendConfirmation.tsx
+++ src/components/user/ResendConfirmation.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import AuthForm, { FieldMappings, emailField } from './AuthForm';
+import AuthForm, { FieldMappings, emailField, AuthData } from './AuthForm';
 import Auth from '../../auth';
 import Notifications from '../../notification';
 
@@ -8,7 +8,7 @@ const EMAIL_KEY: Symbol = Symbol('email');
 export default function ResendConfirmation(): JSX.Element {
     const [submitting, setSubmitting] = useState<boolean>(false);
 
-    function onSubmit(data: Map<string | Symbol, string>): void {
+    function onSubmit(data: AuthData): void {
         let email = data.get(EMAIL_KEY);
         if (email) {
             setSubmitting(true);
