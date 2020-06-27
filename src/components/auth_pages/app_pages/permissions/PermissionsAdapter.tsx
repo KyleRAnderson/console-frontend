22e65d1bce77bdb225fae60eb3109734956de14b
Kyle Anderson <kra.aue@gmail.com>
2020-06-26 23:13:44 -0400

    Add refresh permissions on change
    The permissions adapter now updates the permission that is changed when
    a request is made to change it.
    
    There is a curreny known issue however: it will not update the current
    owner's permission if the owner gets changed, in which case it would
    look as though there are two owners at once until the user refreshses.

--- src/components/auth_pages/app_pages/permissions/PermissionsAdapter.tsx
+++ src/components/auth_pages/app_pages/permissions/PermissionsAdapter.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import GenericPaginated from '../../../GenericPaginated';
-import GenericTable, { PropertyMapping } from '../../../GenericTable';
-import Permission, { ROLES } from '../../../../models/Permission';
-import { PermissionPaginatedResponse, getPermissions } from '../../../../api/permissionAPI';
+import React, { useState } from 'react';
+import PaginatedLoader from '../../../generics/PaginatedLoader';
+import GenericTable, { PropertyMapping } from '../../../generics/GenericTable';
+import Permission, { ROLES, Role } from '../../../../models/Permission';
+import {
+    PermissionPaginatedResponse,
+    getPermissions,
+    updatePermission as patchPermission,
+} from '../../../../api/permissionAPI';
 import { Form } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router-dom';
 import { ROSTER_ID_PARAM } from '../../../../routes/AppPaths';
@@ -10,6 +14,9 @@ import { ROSTER_ID_PARAM } from '../../../../routes/AppPaths';
 type Props = RouteComponentProps<{ [ROSTER_ID_PARAM]: string }>;
 
 export default function PermissionsAdapter(props: Props): JSX.Element {
+    const [changingPermissions, setChangingPermissions] = useState<{ [permissionId: string]: boolean }>({});
+    const [permissions, setPermissions] = useState<Permission[]>([]);
+
     async function getValues(currentPage: number, recordsPerPage?: number): Promise<[Permission[], number]> {
         const response: PermissionPaginatedResponse = await getPermissions(props.match.params[ROSTER_ID_PARAM], {
             page: currentPage,
@@ -18,11 +25,36 @@ export default function PermissionsAdapter(props: Props): JSX.Element {
         return [response.permissions, response.num_pages];
     }
 
+    // Known issue: This will not update the current owner if they promote someone else.
+    function updatePermission(event: React.ChangeEvent<HTMLInputElement>, permission: Permission): void {
+        function setPermissionChanging(permission: Permission, changing: boolean): void {
+            setChangingPermissions({ ...changingPermissions, [permission.id]: changing });
+        }
+
+        setPermissionChanging(permission, true);
+        patchPermission({ ...permission, level: event.currentTarget.value as Role })
+            .then((updated) => {
+                const permissionsCopy = [...permissions];
+                permissionsCopy[permissionsCopy.indexOf(permission)] = updated;
+                setPermissions(permissionsCopy);
+                setPermissionChanging(permission, false);
+            })
+            .catch(() => setPermissionChanging(permission, false));
+    }
+
     function getLevelSelection(permission: Permission): React.ReactNode {
         return (
             <td key={permission.level}>
                 <Form.Group>
-                    <Form.Control as="select" defaultValue={permission.level}>
+                    <Form.Control
+                        as="select"
+                        value={permission.level}
+                        disabled={
+                            /* May have a key in changingPermissions, may be undefined.*/
+                            changingPermissions[permission.id]
+                        }
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => updatePermission(event, permission)}
+                    >
                         {ROLES.map((role) => {
                             return <option key={role}>{role}</option>;
                         })}
@@ -37,9 +69,9 @@ export default function PermissionsAdapter(props: Props): JSX.Element {
         ['Level', getLevelSelection],
     ];
 
-    function getTable(values: Permission[]): React.ReactNode {
-        return <GenericTable<Permission> striped propertyMappings={propertyMappings} values={values} />;
-    }
+    const table: React.ReactNode = (
+        <GenericTable<Permission> striped propertyMappings={propertyMappings} values={permissions} />
+    );
 
-    return <GenericPaginated<Permission> getValues={getValues} table={getTable} />;
+    return <PaginatedLoader<Permission> getValues={getValues} table={table} updateValues={setPermissions} />;
 }
