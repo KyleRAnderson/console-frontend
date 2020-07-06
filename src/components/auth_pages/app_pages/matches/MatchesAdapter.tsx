d8d95de9a2f9a017e539f7d9a373e1d1e98f434a
Kyle Anderson <kra.aue@gmail.com>
2020-07-06 18:03:04 -0400

    Change MatchesAdapter to use LoadUntilReady
    Another component that needs to have access to its load function, so the
    state for this component has been lifted.
    
    Partially in preparation for the later work on filtering that will be
    done on this component.

--- src/components/auth_pages/app_pages/matches/MatchesAdapter.tsx
+++ src/components/auth_pages/app_pages/matches/MatchesAdapter.tsx
@@ -1,32 +1,46 @@
-import React from 'react';
-import { HuntWithProperties } from '../../../../models/Hunt';
-import GenericPaginated, { Props as GenericPaginatedProps } from '../../../generics/GenericPaginated';
-import Match from '../../../../models/Match';
-import { PropertyMapping } from '../../../generics/GenericTable';
+import React, { useEffect, useState } from 'react';
 import { getMatches } from '../../../../api/matchAPI';
+import { HuntWithProperties } from '../../../../models/Hunt';
+import Match from '../../../../models/Match';
+import { Props as GenericPaginatedProps } from '../../../generics/GenericPaginated';
+import GenericTable, { PropertyMapping } from '../../../generics/GenericTable';
+import PaginatedElement from '../../../generics/PaginatedElement';
+import LoadUntilReady from '../../../generics/LoadUntilReady';
+import { createNotification } from '../../../../notification';
 
 type Props = Pick<GenericPaginatedProps<Match>, 'updateSignal'> & {
     hunt: HuntWithProperties;
 };
 
 export default function MatchesAdapter(props: Props): JSX.Element {
-    async function loadMatches(page: number, matchesPerPage?: number): Promise<[Match[], number]> {
-        const data = await getMatches(props.hunt, { page: page, per_page: matchesPerPage });
-        return [data.matches, data.num_pages];
+    const [numPages, setNumPages] = useState<number>(0);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [matches, setMatches] = useState<Match[]>();
+
+    function loadMatches(): void {
+        getMatches(props.hunt, { page: currentPage })
+            .then(({ matches, num_pages: numPages }) => {
+                setNumPages(numPages);
+                setMatches(matches);
+            })
+            .catch(() => createNotification({ message: 'Failed to load matches', type: 'danger' }));
     }
 
+    props.updateSignal?.add(loadMatches);
+    useEffect(loadMatches, [currentPage]);
+
     const propertyMappings: PropertyMapping<Match>[] = [
-        ['First 1', (match) => match.licenses[0].participant.first],
-        ['Last 1', (match) => match.licenses[0].participant.last],
-        ['First 2', (match) => match.licenses[1].participant.first],
-        ['Last 2', (match) => match.licenses[1].participant.last],
+        ['First 1', (match) => match.licenses[0]?.participant?.first || ''],
+        ['Last 1', (match) => match.licenses[0]?.participant?.last || ''],
+        ['First 2', (match) => match.licenses[1]?.participant?.first || ''],
+        ['Last 2', (match) => match.licenses[1]?.participant?.last || ''],
     ];
 
     return (
-        <GenericPaginated
-            updateSignal={props.updateSignal}
-            getValues={loadMatches}
-            propertyMappings={propertyMappings}
-        />
+        <PaginatedElement currentPage={currentPage} numPages={numPages} onSetPage={setCurrentPage}>
+            <LoadUntilReady isLoaded={!!matches}>
+                <GenericTable<Match> striped propertyMappings={propertyMappings} values={matches as Match[]} />
+            </LoadUntilReady>
+        </PaginatedElement>
     );
 }
