045e1f38d89f7427aeb1430536b90db8b98062f3
Kyle Anderson <kra.aue@gmail.com>
2020-07-12 17:35:54 -0400

    Move filter state to URL for participants
    The state of the currently selected hunt on the participant view is now
    stored in the URL instead of as a variable.
    
    This allows for linking to a specific hunt to filter by, which is
    convenient for both the user and for development, since we can now
    create links to a participant table that's been filtered.

--- src/components/auth_pages/app_pages/participants/HuntFilterSelect.tsx
+++ src/components/auth_pages/app_pages/participants/HuntFilterSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Form } from 'react-bootstrap';
 import FormControlElement from '../../../../FormControlElement';
 import Hunt from '../../../../models/Hunt';
@@ -6,25 +6,21 @@ import Hunt from '../../../../models/Hunt';
 export type Props = {
     /** List of hunts to be displaying options for. */
     hunts: Hunt[];
+    /** Currently selected hunt. */
+    currentSelection: Hunt | undefined;
     /** Function to call when the selection changes. */
     onChoose: (hunt?: Hunt) => void;
 };
 
 export default function HuntFilterSelect(props: Props): JSX.Element {
-    const [currentSelection, setCurrentSelection] = useState<Hunt>();
-
     function handleChange(changeEvent: React.ChangeEvent<FormControlElement>): void {
         const selectedHuntId = changeEvent.target.value;
         const selectedHunt: Hunt | undefined = props.hunts.find((hunt) => hunt.id === selectedHuntId);
-        setCurrentSelection(selectedHunt);
+        props.onChoose(selectedHunt);
     }
 
-    useEffect(() => {
-        props.onChoose(currentSelection);
-    }, [currentSelection]);
-
     return (
-        <Form.Control as="select" onChange={handleChange} value={currentSelection?.id || ''}>
+        <Form.Control as="select" onChange={handleChange} value={props.currentSelection?.id || ''}>
             <option value="">-</option>
             {props.hunts.map((hunt) => {
                 return (
--- src/components/auth_pages/app_pages/participants/ParticipantsView.tsx
+++ src/components/auth_pages/app_pages/participants/ParticipantsView.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Col, Container, Form } from 'react-bootstrap';
+import { useHistory, useLocation } from 'react-router-dom';
 import { ParticipantFilters } from '../../../../api/participantAPI';
 import Hunt from '../../../../models/Hunt';
 import Participant from '../../../../models/Participant';
@@ -17,10 +18,38 @@ type Props = {
     hunts?: Hunt[];
 };
 
+// This is here for safety later on if it were to change.
+const QUERY_SEARCH_KEYS: (keyof ParticipantFilters)[] = ['exclude_hunt_id'];
+
 export default function ParticipantsView(props: Props): JSX.Element {
     const [searchQuery, setSearchQuery] = useState<string>();
-    const [filters, setFilters] = useState<ParticipantFilters>({});
     const [upToDate, setUpToDate] = useState<boolean>(false);
+
+    const history = useHistory();
+    const { search } = useLocation();
+    const parsedSearch = new URLSearchParams(search);
+
+    function parseSearchToFilters(): ParticipantFilters {
+        const filtersInSearch: ParticipantFilters = {};
+        for (const key of QUERY_SEARCH_KEYS) {
+            filtersInSearch[key] = parsedSearch.get(key) || undefined;
+        }
+        return filtersInSearch;
+    }
+
+    const filters = parseSearchToFilters();
+
+    function setSearchFromFilters(filters: ParticipantFilters): void {
+        for (const key of QUERY_SEARCH_KEYS) {
+            const value = filters[key];
+            if (value) {
+                parsedSearch.set(key, value);
+            } else {
+                parsedSearch.delete(key);
+            }
+        }
+        history.push({ search: parsedSearch.toString() });
+    }
 
     function reloadParticipants(): void {
         setUpToDate(false);
@@ -42,7 +71,12 @@ export default function ParticipantsView(props: Props): JSX.Element {
                                 <Col lg="4" md="6">
                                     <HuntFilterSelect
                                         hunts={props.hunts}
-                                        onChoose={(hunt) => setFilters({ ...filters, exclude_hunt_id: hunt?.id })}
+                                        onChoose={(hunt) =>
+                                            setSearchFromFilters({ ...filters, exclude_hunt_id: hunt?.id })
+                                        }
+                                        currentSelection={
+                                            props.hunts.find((hunt) => hunt.id === filters.exclude_hunt_id) || undefined
+                                        }
                                     />
                                 </Col>
                                 <Col lg="4" md="6">
