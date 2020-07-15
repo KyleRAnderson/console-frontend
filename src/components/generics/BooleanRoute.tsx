6c51d9e9a7b3a5513e84a80eb169163857880c74
Kyle Anderson <kra.aue@gmail.com>
2020-07-15 00:19:24 -0400

    Improve modularity of BooleanRoute
    BooleanRoute no longer relies on the pathExtension being just a single
    path extension. Previous to this change, you could only have the last
    portion of the path as the pathExtension in BooleanRoute. For
    /test/routes/something, this would be something.
    
    You should now be able to match more than just a single chunk.

--- src/components/generics/BooleanRoute.tsx
+++ src/components/generics/BooleanRoute.tsx
@@ -3,14 +3,16 @@ import { Route, RouteProps } from 'react-router-dom';
 
 type Props = {
     children: (show: boolean) => React.ReactNode;
+    /** The end of the path to match. */
     pathExtension: string;
 } & Omit<RouteProps, 'path'>;
 
 /**
- * A wrapper for the react Route component which
+ * A wrapper for the react Route component which always renders its children but supplies them with a boolean
+ * value for whether or not the full path matches.
  */
 export default function BooleanRoute({ children, pathExtension, ...routeProps }: Props): JSX.Element {
-    const pathRegex = new RegExp(`\/${pathExtension.replace('/', '')}\/?$`);
+    const pathRegex = new RegExp(`\/${pathExtension.replace(/(^\/|\/$)/g, '')}\/?$`);
     return (
         <Route {...routeProps} path={`*/(${pathExtension})?`}>
             {(props) => children(pathRegex.test(props.location.pathname))}
