8cb1085f8120a177f2bddf991c244142e160aa00
Kyle Anderson <kra.aue@gmail.com>
2021-01-11 23:53:50 -0500

    Fix Action Cable route
    The action cable route still assumed that the backend server would be on
    the same hostname.
    
    I've fixed this by using the same environment variable as with axios to
    get the hostname.

--- src/channels/consumer.ts
+++ src/channels/consumer.ts
@@ -4,11 +4,12 @@
 
 import { createConsumer } from '@rails/actioncable';
 import { Cable } from 'actioncable';
+import urljoin from 'url-join';
 import * as ApiPaths from '../routes/ApiPaths';
 
 type AppCable = {
     cable: Cable;
 };
 
-const App: AppCable = { cable: createConsumer(ApiPaths.CABLE_PATH) };
+const App: AppCable = { cable: createConsumer(urljoin(process.env.REACT_APP_API_BASE_URL || '', ApiPaths.CABLE_PATH)) };
 export default App;
